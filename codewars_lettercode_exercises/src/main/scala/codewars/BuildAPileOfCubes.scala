d85fea6729e7c30e0dd72eac8deee1028ee9ddba
Письменский Максим <pismenskiy@kinoplan.ru>
2022-07-19 17:30:00 +0300

    Codewars | Build a pile of cubes lvl 6

---
+++ codewars_lettercode_exercises/src/main/scala/codewars/BuildAPileOfCubes.scala
@@ -0,0 +1,15 @@
+package codewars
+
+object BuildAPileOfCubes {
+  def currentCube(n: Int): Long = Math.pow(n, 3).toLong
+
+  def go(m: Long, n: Int): Int = m match{
+    case m if m > 0 => go(m - currentCube(n), n + 1)
+    case 0 => n - 1
+    case _ => -1
+  }
+
+  def findNb(m: Long): Int = {
+    go(m, 1)
+  }
+}
