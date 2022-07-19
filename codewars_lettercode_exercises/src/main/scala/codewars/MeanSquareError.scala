8fd893b66c72e43fad750555b94e9f9a0fd85e08
Письменский Максим <pismenskiy@kinoplan.ru>
2022-07-19 14:55:24 +0300

    Codewars | Mean Square Error | lvl 5

---
+++ codewars_lettercode_exercises/src/main/scala/codewars/MeanSquareError.scala
@@ -0,0 +1,16 @@
+package codewars
+
+object MeanSquareError {
+  def solution(a: Array[Int], b: Array[Int]): Double = {
+    a.foldLeft((0, Array[Int]())){
+      case ((ind, res), el) =>
+        (ind + 1, res :+ math.pow(el - b(ind), 2).toInt)
+    }._2.foldLeft(0.0)(_ + _) / a.length
+  }
+
+  def solution2(a: List[Int], b: List[Int]): Double =
+    a.lazyZip(b).map{ case (x, y) => math.pow(x - y, 2) }.sum / a.length
+
+  def solution3(a: Array[Int], b: Array[Int]): Double =
+    a.zip(b).map { case (x, y) => math.pow(x-y, 2) }.sum / a.length
+}
