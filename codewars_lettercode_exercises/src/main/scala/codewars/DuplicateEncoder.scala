5c0916acf2681850ee31c9e36e07cc2d10704b77
Письменский Максим <pismenskiy@kinoplan.ru>
2022-07-19 17:09:49 +0300

    Codewars | Duplicate Encoder lvl 6

---
+++ codewars_lettercode_exercises/src/main/scala/codewars/DuplicateEncoder.scala
@@ -0,0 +1,16 @@
+package codewars
+
+object DuplicateEncoder {
+  def duplicateEncode(word: String): String = word match {
+      case "" => ""
+      case w =>
+        val charCounter = word.groupBy(c => c.toLower).view.mapValues(str => str.length)
+        w.foldLeft("")((acc, c) => if(charCounter(c.toLower) > 1) acc + ")" else acc + "(")
+  }
+
+  def solution2(word: String): String = {
+    val wordLower = word.toLowerCase
+    val charCounts = wordLower.groupBy(identity).view.mapValues(_.length)
+    wordLower.map(c => if (charCounts(c) > 1) ')' else '(')
+  }
+}
