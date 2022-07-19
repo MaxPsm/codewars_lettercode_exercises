83bada22145dafaa0bd31c97cbfe96e9ffebe591
Письменский Максим <pismenskiy@kinoplan.ru>
2022-07-19 17:19:19 +0300

    Codewars | Psychic lvl 3

---
+++ codewars_lettercode_exercises/src/main/scala/codewars/Psychic.scala
@@ -0,0 +1,19 @@
+import java.util.Random
+
+object Psychic {
+  def guess(): Double = random.nextDouble
+
+  private val seed: Long = System.currentTimeMillis
+  private val random = new Random(seed)
+
+  private def overrideJavaRandomClass: Unit = {
+    val randomGeneratorClass = Class.forName("java.lang.Math$RandomNumberGeneratorHolder")
+    val randomNumberGeneratorMethod = randomGeneratorClass.getDeclaredField("randomNumberGenerator")
+    randomNumberGeneratorMethod.setAccessible(true)
+
+    val fakeRandomNumberGeneratorMethod = randomNumberGeneratorMethod.get(null).asInstanceOf[Random]
+    fakeRandomNumberGeneratorMethod.setSeed(seed)
+  }
+
+  overrideJavaRandomClass
+}
