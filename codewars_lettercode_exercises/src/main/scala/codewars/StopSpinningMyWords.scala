4691aa39f03b026fbdb2a1bea5ac46e4cb8b31e6
Письменский Максим <pismenskiy@kinoplan.ru>
2022-07-19 17:31:08 +0300

    Codewars | Stop spinning my words lvl 6

---
+++ codewars_lettercode_exercises/src/main/scala/codewars/StopSpinningMyWords.scala
@@ -0,0 +1,10 @@
+package codewars
+
+object StopSpinningMyWords {
+  def spinWords(sentence: String): String = {
+    sentence.split(" ").map(w =>
+      if(w.length() >= 5) w.reverse
+      else w
+    ).mkString(" ")
+  }
+}
